bcb73e00a44b9e11c21e6009a3c6baf318d410d3
Aleksandr Kundelev <enzo.sterro@gmail.com>
2017-07-14 13:45:46 +0300

    Core cleanup.

--- Sitemap to Scenario/AppDelegate.swift
+++ Sitemap to Scenario/AppDelegate.swift
@@ -11,12 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-    }
-    
+	
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
